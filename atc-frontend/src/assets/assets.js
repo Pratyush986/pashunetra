ebad49061cc76e9699a48dce3ddb220bb7a3fe97
Pratyush986 <pratyushsahoo386@gmail.com>
2025-09-17 02:44:33 +0530

    Add files via upload

---
+++ atc-frontend/src/assets/assets.js
@@ -0,0 +1,35 @@
+import arrowIcon from './arrowIcon.svg'
+import exclusiveOfferCardImg1 from "./exclusiveOfferCardImg1.png";
+import exclusiveOfferCardImg2 from "./exclusiveOfferCardImg2.png";
+import exclusiveOfferCardImg3 from "./exclusiveOfferCardImg3.png";
+import logo3 from "./logo3.svg"
+import logoo from "./logoo.jpg"
+import cow1 from "./cow1.jpg"
+import cow2 from "./cow2.jpg"
+import cow3 from "./cow3.jpg"
+export const assets = {
+    arrowIcon,
+    logo3,
+    logoo
+}
+
+export const featuresData = [
+    {
+        _id: 1,
+        title: "Objective Scoring",
+        description: "AI-driven scoring eliminates observer bias and fatigue-driven errors, ensuring reproducible results.",
+        image: cow1
+    },
+    {
+        _id: 2,
+        title: "Image-based Analysis",
+        description: "Extracts body structure parameters like body length, withers height, chest width, and rump angle from cattle images.",
+        image:cow2
+    },
+    {
+        _id: 3,
+        title: "BPA Integration",
+        description: "Seamless auto-recording into the Bharat Pashudhan App at the time of evaluation.",
+        image: cow3
+    }
+];
